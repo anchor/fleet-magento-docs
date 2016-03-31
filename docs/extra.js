5c248d55e5495116e53a5dad7388147a24f031b7
Jez Kabanoff <jez@anchor.net.au>
2016-03-31 12:00:45 +1100

    some fixes to collapsable navigation

--- docs/extra.js
+++ docs/extra.js
@@ -9,13 +9,13 @@ function collapseSidebar() {
 
   sections.each(function(idx, section) {
     let isActive = $('.current', section).length > 0;
-    let control = $('<a class="collapse-control"><span class="collapsed">+</span><span class="expanded">-</span></a>');
+    let control = $('<a class="collapse-control"><span class="collapsed">&#x25bc;</span><span class="expanded">&#x25ac;</span> '+$(' > ul > :first-child > span', section).html()+'</a>');
     control.click(handleToggleSection);
     if (!isActive) {
       control.addClass('collapsed');
       $('> ul', section).addClass('collapsed');
     }
-    $(' > ul > :first-child > span', section).prepend(control);
+    $(' > ul > :first-child > span', section).replaceWith(control);
   });
 
 }
@@ -31,6 +31,6 @@ function handleToggleSection(event) {
 
   // Collapse all children when we collapse the parent
   if (isCollapsed) {
-    $('ul.subnav').addClass('collapsed');
+    $('ul.subnav', this.parentNode).addClass('collapsed');
   }
 }
